94971e6aa5695919a4855e89ca98f885956f10cc
dsudo <dsudo0624@gmail.com>
2018-09-03 18:57:11 +0900

    Improve sorting to receive compare function

--- haskell/samples/sort.hs
+++ haskell/samples/sort.hs
@@ -2,17 +2,17 @@ module Main where
 
   main :: IO ()
   main = do
-    putStrLn "> quick [7, 37, 11, 41, 19, 23, 5, 13, 43, 31, 2, 47, 29, 3, 17]"
-    print $ quick [7, 37, 11, 41, 19, 23, 5, 13, 43, 31, 2, 47, 29, 3, 17]
+    putStrLn "> quick (\\x y -> x - y) [7, 37, 11, 41, 19, 23, 5, 13, 43, 31, 2, 47, 29, 3, 17]"
+    print $ quick (\x y -> x - y) [7, 37, 11, 41, 19, 23, 5, 13, 43, 31, 2, 47, 29, 3, 17]
 
 
-  quick [] = []
-  quick (pivot:rest) =
-    quick (smaller) ++ [pivot] ++ quick (larger)
+  quick fn [] = []
+  quick fn (pivot:rest) =
+    quick fn smaller ++ [pivot] ++ quick fn larger
     where
       (smaller, larger) = partition (pivot, rest, [], [])
       partition (_, [], smaller, larger) = (smaller, larger)
       partition (pivot, (h:t), smaller, larger) =
-        if h <= pivot
+        if (fn h pivot) <= 0
           then partition(pivot, t, smaller ++ [h], larger)
           else partition(pivot, t, smaller, larger ++ [h])
